bcf1f6e8c49f4b1291a49b13e99661e9ca3b763d
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-05-08 00:57:18 -0700

    add totalSupply test case

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,29 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/Erc4626.sol";
+import "solmate/tokens/ERC20.sol";
+import "forge-std/console.sol";
 
-contract ContractTest is Test {
-    function setUp() public {}
+contract MockERC20 is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {}
+}
 
-    function testExample() public {
-        assertTrue(true);
+contract Erc4626Test is Test {
+    Vault public vault;
+    ERC20 public underlying;
+
+    function setUp() public {
+        underlying = new MockERC20("TEST", "TST", 18);
+        vault = new Vault(address(underlying), "TESTVAULT", "vTST", 18);
+    }
+
+    function testTotalSupplyShares() public view {
+        console.log(vault.totalSupply());
+        require(vault.totalSupply() == 0);
     }
 }
