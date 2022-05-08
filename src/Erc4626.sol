ebfefeda705d5706d57768931c7c520783e4fe36
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2022-05-08 00:36:25 -0700

    add erc20 logic

--- src/Erc4626.sol
+++ src/Erc4626.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Vault {}
+import "solmate/tokens/ERC20.sol";
+
+contract Vault is ERC20 {
+    ERC20 public asset;
+
+    constructor(
+        address _erc20,
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals
+    ) ERC20(_name, _symbol, _decimals) {
+        asset = ERC20(_erc20);
+    }
+
+    function totalAssets() public view returns (uint256) {
+        return asset.balanceOf(address(this));
+    }
+}
